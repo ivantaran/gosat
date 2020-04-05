045d98f4e84fbfc6d73ec7863eeb5e77f0d8313a
ri <ri@scaegroup.com>
2020-04-05 12:33:40 +0300

    initl complete

--- sgp4.go
+++ sgp4.go
@@ -1,5 +1,13 @@
 package gosat
 
+import "math"
+
+//Constants
+const (
+	TwoPi   = math.Pi * 2.0
+	Deg2Rad = math.Pi / 180.0
+)
+
 type elsetrec struct {
 	satnum        int // TODO: long int originally
 	epochyr       int
@@ -144,4 +152,135 @@ type elsetrec struct {
 	active      bool    //"Active S/C" flag (0=n, 1=y)
 	not_orbital bool    //"Orbiting S/C" flag (0=n, 1=y)
 	rcs_m2      float64 //"RCS (m^2)" storage
+}
+
+type tle struct {
+	satn   int
+	epoch  float64
+	xbstar float64
+	xndot  float64
+	xnddot float64
+	xecco  float64
+	xargpo float64
+	xinclo float64
+	xmo    float64
+	xno    float64
+	xnodeo float64
+}
+
+/*-----------------------------------------------------------------------------
+*
+*                           procedure initl
+*
+*  this procedure initializes the spg4 propagator. all the initialization is
+*    consolidated here instead of having multiple loops inside other routines.
+*
+*  author        : david vallado                  719-573-2600   28 jun 2005
+*
+*  inputs        :
+*    satn        - satellite number - not needed, placed in satrec
+*    xke         - reciprocal of tumin
+*    j2          - j2 zonal harmonic
+*    ecco        - eccentricity                           0.0 - 1.0
+*    epoch       - epoch time in days from jan 0, 1950. 0 hr
+*    inclo       - inclination of satellite
+*    no          - mean motion of satellite
+*
+*  outputs       :
+*    ainv        - 1.0 / a
+*    ao          - semi major axis
+*    con41       -
+*    con42       - 1.0 - 5.0 cos(i)
+*    cosio       - cosine of inclination
+*    cosio2      - cosio squared
+*    eccsq       - eccentricity squared
+*    method      - flag for deep space                    'd', 'n'
+*    omeosq      - 1.0 - ecco * ecco
+*    posq        - semi-parameter squared
+*    rp          - radius of perigee
+*    rteosq      - square root of (1.0 - ecco*ecco)
+*    sinio       - sine of inclination
+*    gsto        - gst at time of observation               rad
+*    no          - mean motion of satellite
+*
+*  locals        :
+*    ak          -
+*    d1          -
+*    del         -
+*    adel        -
+*    po          -
+*
+*  coupling      :
+*    getgravconst- no longer used
+*    gstime      - find greenwich sidereal time from the julian date
+*
+*  references    :
+*    hoots, roehrich, norad spacetrack report #3 1980
+*    hoots, norad spacetrack report #6 1986
+*    hoots, schumacher and glover 2004
+*    vallado, crawford, hujsak, kelso  2006
+----------------------------------------------------------------------------*/
+
+func (s *elsetrec) initl(epoch float64) (ainv float64, sinio float64, con41 float64, posq float64, rp float64) {
+	// float64 xke, float64 j2, float64 ecco, float64 epoch, float64 inclo, float64 no_kozai, char opsmode,
+	// char &method, float64 &ainv, float64 &ao, float64 &con41, float64 &con42, float64 &cosio,
+	// float64 &cosio2, float64 &eccsq, float64 &omeosq, float64 &posq, float64 &rp, float64 &rteosq,
+	// float64 &sinio, float64 &gsto, float64 &no_unkozai
+
+	x2o3 := 2.0 / 3.0
+
+	/* ------------- calculate auxillary epoch quantities ---------- */
+	eccsq := s.ecco * s.ecco
+	omeosq := 1.0 - eccsq
+	rteosq := math.Sqrt(omeosq)
+	cosio := math.Cos(s.inclo)
+	cosio2 := cosio * cosio
+
+	/* ------------------ un-kozai the mean motion ----------------- */
+	ak := math.Pow(s.xke/s.no_kozai, x2o3)
+	d1 := 0.75 * s.j2 * (3.0*cosio2 - 1.0) / (rteosq * omeosq)
+	del := d1 / (ak * ak)
+	adel := ak * (1.0 - del*del - del*(1.0/3.0+134.0*del*del/81.0))
+	del = d1 / (adel * adel)
+	s.no_unkozai = s.no_kozai / (1.0 + del)
+
+	ao := math.Pow(s.xke/s.no_unkozai, x2o3)
+	sinio = math.Sin(s.inclo)
+	po := ao * omeosq
+	con42 := 1.0 - 5.0*cosio2
+	con41 = -con42 - cosio2 - cosio2
+	ainv = 1.0 / ao
+	posq = po * po
+	rp = ao * (1.0 - s.ecco)
+	s.method = 'n'
+
+	ts70 := epoch - 7305.0
+	ds70 := math.Floor(ts70 + 1.0e-8)
+	tfrac := ts70 - ds70
+	// find greenwich location at epoch
+	c1 := 1.72027916940703639e-2
+	thgr70 := 1.7321343856509374
+	fk5r := 5.07551419432269442e-15
+	c1p2p := c1 + TwoPi
+	gsto1 := math.Mod(thgr70+c1*ds70+c1p2p*tfrac+ts70*ts70*fk5r, TwoPi)
+	if gsto1 < 0.0 {
+		gsto1 = gsto1 + TwoPi
+	}
+	s.gsto = gstime(epoch + 2433281.5)
+
+	return
+}
+
+func gstime(jdut1 float64) float64 {
+
+	tut1 := (jdut1 - 2451545.0) / 36525.0
+	temp := -6.2e-6*tut1*tut1*tut1 + 0.093104*tut1*tut1 + (876600.0*3600+8640184.812866)*tut1 + 67310.54841 // seconds
+	temp = math.Mod(temp*Deg2Rad/240.0, TwoPi)                                                              //360/86400 = 1/240, to deg, to rad
+
+	// ------------------------ check quadrants ---------------------
+	if temp < 0.0 {
+		temp += TwoPi
+	}
+
+	return temp
 }
