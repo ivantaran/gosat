6f9eee558eccf730c7b569ae4f9d3e969bf7d631
ri <ri@scaegroup.com>
2020-04-12 11:26:09 +0300

    .

--- sgp4_test.go
+++ sgp4_test.go
@@ -20,13 +20,41 @@ func TestTleLoad(t *testing.T) {
 		t.Error(err)
 	} else {
 		for _, tleItem := range tleList {
-			if tleItem.satn == 11801 {
-				fmt.Print("Deep space, perigee = 82.48 (<98) for s4 > 20 mod")
+			if tleItem.satn == 33334 {
+				fmt.Println("OLOLO")
 			}
 			err = sat.sgp4init("wgs84", tleItem, 'i')
-			if err != nil {
-				fmt.Println(*tleItem)
-				t.Error(err)
+			switch sat.satnum {
+			case 33333:
+				var r [6]float64
+				err := sat.sgp4(3600.0, r[0:3], r[3:])
+				if err == nil || sat.error != 4 {
+					fmt.Println(*tleItem)
+					t.Errorf("[FAIL] errnum:%d satnum:%d \"%s\"", sat.error, sat.satnum, err)
+				} else {
+					t.Logf("[ OK ] errnum:%d satnum:%d \"%s\"", sat.error, sat.satnum, err)
+				}
+			case 33334:
+				if err == nil || sat.error != 3 {
+					fmt.Println(*tleItem)
+					t.Errorf("[FAIL] errnum:%d satnum:%d \"%s\"", sat.error, sat.satnum, err)
+				} else {
+					t.Logf("[ OK ] errnum:%d satnum:%d \"%s\"", sat.error, sat.satnum, err)
+				}
+			// case 33335: Wrong test case
+			// 	var r [6]float64
+			// 	err := sat.sgp4(3600.0, r[0:3], r[3:])
+			// 	if err == nil || sat.error != 3 {
+			// 		fmt.Println(*tleItem)
+			// 		t.Errorf("[FAIL] errnum:%d satnum:%d \"%s\"", sat.error, sat.satnum, err)
+			// 	} else {
+			// 		t.Logf("[ OK ] errnum:%d satnum:%d \"%s\"", sat.error, sat.satnum, err)
+			// 	}
+			default:
+				if err != nil {
+					fmt.Println(*tleItem)
+					t.Errorf("[FAIL] errnum:%d satnum:%d \"%s\"", sat.error, sat.satnum, err)
+				}
 			}
 		}
 	}
