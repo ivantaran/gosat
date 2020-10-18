c7d0a649376285ab92d9fecf13a661344a5eb3f7
ri <ri@scaegroup.com>
2020-10-19 00:33:18 +0300

    eci2ecef

--- pkg/gosat/extendedsatellite.go
+++ pkg/gosat/extendedsatellite.go
@@ -10,6 +10,23 @@ type extendedSatellite struct {
 	Track [128][6]float64
 }
 
+// eci2ecef Conversion from Earth-centered inertial (ECI) to Earth-centered, Earth-Fixed (ECEF)
+func eci2ecef(eci [6]float64, angle float64) (ecef [6]float64) {
+	// angular speed of Earth's rotation rad/s
+	angularSpeed := 7.2921151467e-5
+	// alpha := angularSpeed*minutes()*60.0 + gsto()
+	// alpha := 0.0
+	sina := math.Sin(angle)
+	cosa := math.Cos(angle)
+	ecef[0] = cosa*eci[0] + sina*eci[1]
+	ecef[1] = -sina*eci[0] + cosa*eci[1]
+	ecef[2] = eci[2]
+	ecef[3] = cosa*eci[3] + sina*eci[4] + angularSpeed*ecef[1]
+	ecef[4] = -sina*eci[3] + cosa*eci[4] - angularSpeed*ecef[0]
+	ecef[5] = eci[5]
+	return
+}
+
 func (es *extendedSatellite) Update(t time.Time) error {
 	timeStartSeconds := -60.0 * math.Pi / es.no
 	timeStepSeconds := 120.0 * math.Pi / es.no / float64(len(es.Track))
@@ -17,10 +34,18 @@ func (es *extendedSatellite) Update(t time.Time) error {
 	timeStep := time.Duration(float64(time.Second) * timeStepSeconds)
 	trackTime := t.Add(timeStart)
 	for i := range es.Track {
+		angle := float64(trackTime.Hour()*3600 + trackTime.Minute()*60 + trackTime.Second())
+		angle += float64(trackTime.Nanosecond()) * 1.0e-9
+		angle *= 7.2921151467e-5
 		es.satellite.Update(trackTime)
-		es.Track[i] = es.Coords
+		es.Track[i] = eci2ecef(es.Coords, angle)
 		trackTime = trackTime.Add(timeStep)
 	}
+	// TODO make seconds of day func
 	err := es.satellite.Update(t)
+	angle := float64(t.Hour()*3600 + t.Minute()*60 + t.Second())
+	angle += float64(t.Nanosecond()) * 1.0e-9
+	angle *= 7.2921151467e-5
+	es.Coords = eci2ecef(es.Coords, angle)
 	return err
 }
